6d3500fb98680896eb84b6c2100bca5144d14414
diana <dianagaripova58@gmail.com>
2024-03-27 17:56:05 +0500

    kkkk

--- migrations/100-create-courses-table.js
+++ migrations/100-create-courses-table.js
@@ -8,12 +8,28 @@ exports.up = pgm => {
             primaryKey:true,
         },
         courseName:{
-            type:'varchar(255)',
-            unique: true
+            type:'varchar(255)'
         },
         courseDescription:{
             type:'varchar(1500)'
-        }
+        },
+        numberOfHours:{
+            type:'integer'
+        },
+        schedule:{
+            type: 'varchar(255)'
+        },
+        createDate:{
+            type:'timestamp with time zone',
+            default:pgm.func('now()')
+        },
+        updateDate:{
+            type:'timestamp with time zone',
+            default:pgm.func('now()')
+        },
+    }, {
+        ifNotExists: true,
+        comment:'Таблица, где хранится информация о курсах'
     })
 };
 exports.down = pgm => {
