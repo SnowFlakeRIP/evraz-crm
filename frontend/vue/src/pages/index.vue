b7dd0dfe3a06c80bc6695c9f07cc314a4b4a55f2
filipp_fazliev <fil.fazliyev@mail.ru>
2024-04-01 17:57:21 +0500

    фронт

---
+++ frontend/vue/src/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script setup>
+let name = ref('');
+let date = ref(null)
+let dateStart = ref(null)
+let dateFinish = ref(null)
+let number = ref(null)
+
+let documents = ref([
+  {
+    name: '',
+    date: null,
+    dateStart: null,
+    dateFinish: null,
+    number: null
+  }])
+</script>
