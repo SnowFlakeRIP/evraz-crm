68717a022a49106e4fd7c75b199168168233077e
vlad_dauer <vlad.dauermein@gmail.com>
2024-04-01 17:59:56 +0500

    Создание страницы расписания, добавление основных элементов (календарь, кнопка добавления, сортировки)

---
+++ frontend/schedule/src/pages/schedule.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="left-menu">
+    <v-btn prepend-icon="$vuetify">
+      Добавить
+    </v-btn>
+    <v-date-picker></v-date-picker>
+    <div id="sorts"></div>
+    Здесь будут сортировки
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+function hideLeftMenu() {
+
+}
+</script>
+
+<style>
+#head {
+  background-color: lightgray;
+  height: 10%;
+  width: 100%;
+}
+</style>
