b06af91282e8f8ebe8adab7de62d165d5d70f0ac
diana <dianagaripova58@gmail.com>
2024-04-01 18:05:13 +0500

    kkkk

---
+++ frontend/course/src/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+<div>helo</div>
+  <v-btn variant="outlined" to="/createCourse">
+    создать курсик
+  </v-btn>
+
+</template>
+
+<script setup>
+  //
+</script>
---
+++ frontend/course/src/router/index.js
@@ -0,0 +1,34 @@
+/**
+ * router/index.ts
+ *
+ * Automatic routes for `./src/pages/*.vue`
+ */
+
+// Composables
+import { createRouter, createWebHistory } from 'vue-router/auto'
+// import { setupLayouts } from 'virtual:generated-layouts'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  // extendRoutes: setupLayouts,
+  routes: [
+    {
+      path: '/',
+      component: () => import('../layouts/default.vue'),
+      children: [
+        {
+          path: '',
+          name: 'index',
+          component: () => import('../pages/index.vue'),
+        },
+        {
+          path: 'createCourse',
+          name: 'page1',
+          component: () => import('../pages/createCourse.vue'),
+        }
+      ]
+    }
+]
+})
+
+export default router
