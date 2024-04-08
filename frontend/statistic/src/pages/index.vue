c174a31ba63e04bd229514cc03a930ffd1e67538
vikavorobyeva <my-miloji@bk.ru>
2024-04-08 17:57:46 +0500

    Ehfhff

--- frontend/statistic/src/pages/index.vue
+++ frontend/statistic/src/pages/index.vue
@@ -2,7 +2,7 @@
   <h3 class="name">Статистика</h3>
   <div class="variants">
     <input id="info__body_1" class="info__switch" type="checkbox">
-    <label for="info__body_1" class="info__headline">Вебинары, которые помогут блеснуть эрудицией!</label>
+    <label for="info__body_1" class="info__headline">активности пользователей</label>
     <div class="info__body">
       Вебинары образовательного проекта Level One — современный формат интеллектуального досуга, который будет интересен всем, кто хочет узнать что-то новое. Всего за пару часов вы совершите путешествие в увлекательный мир науки и искусства. Талантливые лекторы
       поделятся своими знаниями, доступно расскажут о важных научных открытиях и расширят ваш кругозор.
@@ -15,16 +15,13 @@
 </script>
 
 <style>
+
 .name{
   padding: 20px;
   border: 1px solid #ababab;
   box-shadow: 0 0 5px rgba(0, 89, 68 , 60%);
   border-radius: 20px;
   font-style: italic;
-}
-.info {
-  background-color: #f9f9ff;
-  padding: 10px 25px;
 }
 
 .info__switch {
@@ -39,34 +36,39 @@
 
 .info__switch:checked~.info__body {
   max-height: 100px;
-}
-
-.info__headline::before {
-  content: "";
+  padding: 10px;
+  border: 1px solid darkgreen;
+  border-top: none;
+  border-radius: 10px;
+  border-top-right-radius: 0;
+  border-top-left-radius: 0;
   position: absolute;
-  left: -18px;
-  top: 4px;
-  border: 6px solid #39739d;
-  border-bottom-color: red transparent;
-  border-right-color: transparent;
-  border-left-color: transparent;
-}
-
-.info__switch:checked~.info__headline::before {
-  transform: rotate(180deg) translateY(6px);
 }
 
 .info__headline {
-  display: block;
+  display: flex;
   position: relative;
-  margin-bottom: 15px;
+  margin-top: 15px;
+  padding: 15px;
+  border: 1px solid #e3e3e3;
+  border-radius: 10px;
+  transition: 0.4s;
   user-select: none;
-  color: #39739d;
+  color: #000000;
+  background-color: #e3e3e3;
   font-weight: normal;
   cursor: pointer;
 }
 
+.info__switch:checked~.info__headline{
+  transition: 0.2s;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
 .info__headline:hover {
   text-decoration: underline;
+  color: #3fa9ff;
+  background-color: #dadada;
 }
 </style>
