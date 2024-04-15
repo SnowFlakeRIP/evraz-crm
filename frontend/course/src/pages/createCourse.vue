69dd662d817b42135bb3ddd4b3dec924be9e1e0a
diana <dianagaripova58@gmail.com>
2024-04-15 17:55:23 +0500

    kkkk

--- frontend/course/src/pages/createCourse.vue
+++ frontend/course/src/pages/createCourse.vue
@@ -1,11 +1,89 @@
-<script setup>
-
+<script >
+export default {
+  data: () => ({
+    rules: {
+      required: value => !!value || 'Заполните поле',
+    },
+  }),
+}
 </script>
 
 <template>
-<div>сщздацте курс</div>
+  <div class="row">
+    <h1>Создайте курс</h1>
+    <div class="fields">
+      <div class="field">
+        <v-responsive
+          class="mx-auto"
+          max-width="344"
+
+        >
+          <v-text-field
+            class="block"
+            :rules="[rules.required]"
+
+            label="Название"
+            variant="outlined"
+
+          ></v-text-field>
+        </v-responsive>
+      </div>
+      <div >
+        <v-responsive
+          class="mx-auto"
+          max-width="344"
+        >
+          <v-textarea
+            class="block"
+            :rules="[rules.required]"
+            bg-color="amber-lighten-4"
+            color="orange orange-darken-4"
+            label="Описание"
+            variant="outlined"
+          ></v-textarea>
+        </v-responsive>
+      </div>
+      <div>
+        <v-responsive
+          class="mx-auto"
+          max-width="344"
+        >
+          <v-text-field
+            class="block"
+            :rules="[rules.required]"
+            hide-details="auto"
+            label="Расписание"
+            variant="outlined"
+            placeholder="Например: пн 16:00-18:00 ср 16:00-18:00"
+            type="email"
+          ></v-text-field>
+        </v-responsive>
+      </div>
+      <div>
+        <v-responsive
+          class="mx-auto"
+          max-width="344"
+        >
+          <v-text-field
+            class="block"
+            :rules="[rules.required]"
+            hide-details="auto"
+            label="Название"
+            variant="outlined"
+            type="number"
+
+          ></v-text-field>
+
+        </v-responsive>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped lang="sass">
+<style>
+.block {
+  padding-top: 35px;
+  }
+
 
 </style>
