8afdd6eb09621eb1b42edc390f38ec54e2d7b18c
талантик <oknoigorik@mail.ru>
2024-04-15 17:45:18 +0500

    initial commit

---
+++ frontend/vuetify-project/src/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-btn variant="outlined" to="/pageReferalProgramm">
+    Реферальная программа
+  </v-btn>
+  <v-btn variant="outlined" to="/pageMyBonusSestema" >
+    Моя накопительная система
+  </v-btn>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter();
+</script>
