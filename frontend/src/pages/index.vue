663cb7d84765fcb47e65b917401338bd8a8f3e35
keletsu <itswrittenwithanunderscore@gmail.com>
2024-04-17 17:53:00 +0500

    переработан календарь

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -7,52 +7,40 @@
       <v-text-field label="Поиск" hide-details class="ma-2"/>
     </v-toolbar>
     <v-main style="display: flex; flex-direction: row; height: calc(100vh - 64px); padding-bottom: 0">
-      <div id="options" style="padding: 4px 0">
-        <v-btn variant="outlined">Добавить занятие</v-btn>
-        <v-date-picker show-adjacent-months hide-header min="2023-01-01" max="2024-12-31" v-model="value"></v-date-picker>
+      <div id="options" style="padding: 8px; display: flex; flex-direction: column; gap: 8px;">
+        <v-dialog max-width="500">
+          <template v-slot:activator="{ props: activatorProps }">
+            <v-btn
+              v-bind="activatorProps"
+              color="surface-variant"
+              text="Добавить занятие"
+              variant="outlined"
+            ></v-btn>
+          </template>
+          <template v-slot:default="{ isActive }">
+            <v-card title="Добавить занятие">
+              <v-text-field label="Название" required :model-value="name"></v-text-field>
+              <v-text-field label="Начало" required></v-text-field>
+              <v-text-field label="Конец" required></v-text-field>
+              <v-text-field label="ХЗ" required></v-text-field>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  text="Отмена"
+                  @click="isActive.value = false"
+                ></v-btn>
+                <v-btn
+                  text="Добавить"
+                  @click="makeEvent(); isActive.value = false"
+                ></v-btn>
+              </v-card-actions>
+            </v-card>
+          </template>
+        </v-dialog>
         <v-card>Сортировки</v-card>
       </div>
-<!--      <div id="calendar" style="flex: 1; overflow-y: scroll">-->
-<!--        <v-container fluid style="padding: 0; height: max-content">-->
-<!--          <v-row no-gutters v-for="i in 25" style="flex-wrap: nowrap">-->
-<!--            <v-col-->
-<!--              v-for="n in 8"-->
-<!--              :key="n"-->
-<!--              cols="7"-->
-<!--              sm="2"-->
-<!--            >-->
-<!--              <v-sheet class="pa-2" style="position: relative; border: 1px solid gray">-->
-<!--                <div v-if="i === 1 && n === 1">-->
-<!--                  <span style="position: absolute; top: 2px; right: 4px">Дата</span>-->
-<!--                  <span style="position: absolute; bottom: 2px; left: 4px">Время</span>-->
-<!--                  <span style="visibility: hidden">Суперклей</span>-->
-<!--                </div>-->
-<!--                <div v-else-if="n === 1">-->
-<!--                  {{i-1}}:00-->
-<!--                </div>-->
-<!--                <div v-else-if="i === 1">-->
-<!--                  День {{n-1}}-->
-<!--                </div>-->
-<!--                <div v-else>-->
-<!--                  Чтото-->
-<!--                </div>-->
-<!--              </v-sheet>-->
-<!--            </v-col>-->
-<!--          </v-row>-->
-<!--        </v-container>-->
-<!--      </div>-->
-      <div id="calendar" style="flex: 1; overflow-y: scroll">
-        <v-sheet>
-          <v-calendar
-            ref="calendar"
-            v-model="value"
-            :events="events"
-            view-mode="week"
-            :weekdays="weekday"
-            min="2023-01-01"
-            max="2024-12-31"
-          ></v-calendar>
-        </v-sheet>
+      <div id="calendar" style="flex: 1; overflow-y: scroll; padding: 8px">
+        <ScheduleXCalendar :calendar-app="calendarApp" />
       </div>
     </v-main>
 </template>
@@ -65,71 +53,59 @@
 </style>
 
 <script setup>
+import { ScheduleXCalendar } from '@schedule-x/vue'
+import { createDragAndDropPlugin } from '@schedule-x/drag-and-drop'
+import { createResizePlugin } from "@schedule-x/resize";
+import { createEventModalPlugin } from "@schedule-x/event-modal";
+import {
+  createCalendar,
+  viewDay,
+  viewWeek,
+  viewMonthGrid,
+  viewMonthAgenda,
+} from '@schedule-x/calendar'
+import '@schedule-x/theme-default/dist/index.css'
 
-</script>
-
-<script>
-
-import {computed, ref} from 'vue';
-import {useDate} from "vuetify";
-
-export default {
-  data: () => ({
-    type: 'day',
-    types: ['month', 'week', 'day'],
-    weekday: [1, 2, 3, 4, 5, 6],
-    weekdays: [
-      { title: 'Sun - Sat', value: [0, 1, 2, 3, 4, 5, 6] },
-      { title: 'Mon - Sun', value: [1, 2, 3, 4, 5, 6, 0] },
-      { title: 'Mon - Fri', value: [1, 2, 3, 4, 5] },
-      { title: 'Mon, Wed, Fri', value: [1, 3, 5] },
-    ],
-    value: ref([new Date()]),
-    events: [],
-    colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
-    titles: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
-  }),
-  mounted () {
-    const adapter = useDate()
-    this.getEvents({ start: adapter.startOfDay(adapter.startOfMonth(new Date())), end: adapter.endOfDay(adapter.endOfMonth(new Date())) })
-  },
-  computed: {
-
-  },
-  methods: {
-    getEvents ({ start, end }) {
-      const events = []
-
-      const min = start
-      const max = end
-      const days = (max.getTime() - min.getTime()) / 86400000
-      const eventCount = this.rnd(days, days + 20)
-
-      for (let i = 0; i < eventCount; i++) {
-        const allDay = this.rnd(0, 3) === 0
-        const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-        const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-        const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-        const second = new Date(first.getTime() + secondTimestamp)
-
-        events.push({
-          title: this.titles[this.rnd(0, this.titles.length - 1)],
-          start: first,
-          end: second,
-          color: this.colors[this.rnd(0, this.colors.length - 1)],
-          allDay: !allDay,
-        })
-      }
-
-      this.events = events
+let name = ref("jkfjks")
+let value = ref(new Date())
+const calendarApp = createCalendar({
+  selectedDate: '2024-04-17',
+  locale: "ru-RU",
+  views: [viewDay, viewWeek, viewMonthGrid, viewMonthAgenda],
+  defaultView: viewWeek.name,
+  events: (function () {
+    let ev = []
+    for (let i = 0; i < 5; i++) {
+      ev.push({
+        id: i,
+        start: `2024-04-17 ${(i%24).toString().padStart(2,"0")}:00`,
+        end: `2024-04-17 ${((i+1)%24).toString().padStart(2,"0")}:00`,
+        title: `aaa ${i}`
+      })
+    }
+    return ev
+  }) (),
+  callbacks: {
+    onRangeUpdate(range) {
+      value.value = new Date(range.start)
     },
-    getEventColor (event) {
-      return event.color
-    },
-    rnd (a, b) {
-      return Math.floor((b - a + 1) * Math.random()) + a
+    onEventClick(calendarEvent) {
+      console.log('onEventClick', calendarEvent)
     },
   },
+  plugins: [
+    createDragAndDropPlugin(), createResizePlugin(), createEventModalPlugin()
+  ]
+})
+
+function makeEvent() {
+  let time = Math.floor(Math.random()*24)
+  let id = Math.floor(Math.random()*10000)
+  calendarApp.events.add({
+    id,
+    start: `2024-04-17 ${(time%24).toString().padStart(2,"0")}:00`,
+    end: `2024-04-17 ${((time+1)%24).toString().padStart(2,"0")}:00`,
+    title: `aaa ${id}`
+  })
 }
-
 </script>
