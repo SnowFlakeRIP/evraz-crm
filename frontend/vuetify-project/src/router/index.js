27d707a7bc0f84a04c7be06f7527111d06b4678e
талантик <oknoigorik@mail.ru>
2024-04-22 14:19:39 +0500

    initial commit

--- frontend/vuetify-project/src/router/index.js
+++ frontend/vuetify-project/src/router/index.js
@@ -5,38 +5,12 @@
  */
 
 // Composables
-import {
-  createRouter,
-  createWebHistory,
-} from 'vue-router/auto';
-// import { setupLayouts } from 'virtual:generated-layouts'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { setupLayouts } from 'virtual:generated-layouts'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  // extendRoutes: setupLayouts,
-  routes: [
-    {
-      path:      '/',
-      component: () => import('../layouts/default.vue'),
-      children:  [
-        {
-          path:      '',
-          name:      'index',
-          component: () => import('../pages/index.vue'),
-        },
-        {
-          path:      'page1',
-          name:      'page1',
-          component: () => import('../pages/pageReferalProgramm.vue'),
-        },
-        {
-          path:      'page2',
-          name:      'page2',
-          component: () => import('../pages/pageMyBonusSestema.vue'),
-        },
-      ],
-    },
-  ],
-});
+  extendRoutes: setupLayouts,
+})
 
-export default router;
+export default router
