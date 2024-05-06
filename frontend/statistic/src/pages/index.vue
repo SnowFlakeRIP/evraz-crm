a3345337342cb0f5ddd9e2cfec09c07f2d2f2570
vikavorobyeva <my-miloji@bk.ru>
2024-05-06 18:03:04 +0500

    Diagrams

--- frontend/statistic/src/pages/index.vue
+++ frontend/statistic/src/pages/index.vue
@@ -53,16 +53,101 @@
           ></v-combobox>
         </v-col>
         <div class="parentButton">
-          <button @click="diagram" class="button">Создать диаграмму</button>
+          <button @click="diagramUsers" class="button">Создать диаграмму</button>
+        </div>
+        <div v-if="usersActiveChart" class="diagram">
+          <vue-apex-charts type="pie" width="800" :options="usersChart" :series="usersSeries"></vue-apex-charts>
         </div>
       </v-window-item>
 
       <v-window-item value="lessons">
-        Two
+        <v-row>
+          <v-col cols="3">
+            <v-row no-gutters>
+              <v-col cols="12">Укажите период</v-col>
+              <v-col cols="5" class="ma-2">
+                <v-text-field
+                  label="С"
+                  type="date"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="5" class="ma-2">
+                <v-text-field
+                  label="По"
+                  type="date"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+
+        <v-row
+          justify="center"
+          no-gutters
+        >
+          <span>Укажите курс, которому нужно составить диаграмму</span>
+        </v-row>
+
+        <v-col cols="12">
+          <v-combobox
+            :items="['Собака без ножек','Майнкрафт','Роблокс']"
+            density="comfortable"
+            label="Название курса"
+          ></v-combobox>
+          <v-combobox
+            :items="[1,2]"
+            density="comfortable"
+            label="Часть курса"
+          ></v-combobox>
+        </v-col>
+        <div class="parentButton">
+          <button @click="diagramLessons" class="button">Создать диаграмму</button>
+        </div>
       </v-window-item>
 
       <v-window-item value="payments">
-        Three
+        <v-row>
+          <v-col cols="3">
+            <v-row no-gutters>
+              <v-col cols="12">Укажите период</v-col>
+              <v-col cols="5" class="ma-2">
+                <v-text-field
+                  label="С"
+                  type="date"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="5" class="ma-2">
+                <v-text-field
+                  label="По"
+                  type="date"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+
+        <v-row
+          justify="center"
+          no-gutters
+        >
+          <span>Укажите курс, которому нужно составить диаграмму</span>
+        </v-row>
+
+        <v-col cols="12">
+          <v-combobox
+            :items="['Собака без ножек','Майнкрафт','Роблокс']"
+            density="comfortable"
+            label="Название курса"
+          ></v-combobox>
+          <v-combobox
+            :items="[1,2]"
+            density="comfortable"
+            label="Часть курса"
+          ></v-combobox>
+        </v-col>
+        <div class="parentButton">
+          <button @click="diagramPayments" class="button">Создать диаграмму</button>
+        </div>
       </v-window-item>
     </v-window>
   </v-card-text>
@@ -70,17 +155,52 @@
 
 <script setup>
 import {ref} from "vue";
+import VueApexCharts from "vue3-apexcharts";
 
 const tab = ref('users');
 
-function diagram(){
+const usersActiveChart = ref(false);
+const usersSeries = ref([]);
+const usersChart = ref({
+  chart: {
+    width: 380,
+    type: 'pie',
+  },
+  labels: ['Активные', 'Не активные'],
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      chart: {
+        width: 200
+      },
+      legend: {
+        position: 'bottom'
+      }
+    }
+  }]
+});
+const lessonsSeries = ref([]);
+const lessonsChart = ref({});
+const paymentsSeries = ref([]);
+const paymentsChart = ref({});
+
+function diagramUsers() {
+  usersSeries.value = [44, 55];
+  usersActiveChart.value = true;
+}
+
+function diagramLessons() {
+
+}
+
+function diagramPayments() {
 
 }
 </script>
 
 <style>
 
-.name{
+.name {
   margin-bottom: 20px;
   padding: 20px;
   border: 1px solid #ababab;
@@ -88,13 +208,13 @@ function diagram(){
   font-style: italic;
 }
 
-.parentButton{
+.parentButton {
   display: flex;
   justify-content: flex-end;
   margin-bottom: 10px;
 }
 
-.button{
+.button {
   border: 1px solid #131313;
   border-radius: 7px;
   padding: 8px;
