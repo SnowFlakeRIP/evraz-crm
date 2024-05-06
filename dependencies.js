8320cbc4acfeb03b017737408fa2bf5aa6331699
FilFazliev2026 <146732341+FilFazliev2026@users.noreply.github.com>
2024-05-06 16:01:02 +0500

    Delete dependencies.js

--- dependencies.js
+++
@@ -1,8 +0,0 @@
-const { pool } = require('./services/libs/pool');
-const constants = require('./services/constants');
-
-module.exports = {
-    pool:      pool,
-    constants: constants,
-    
-};
